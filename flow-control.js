3bf59d5c93a6d906bd791dae7a22664140d3445e
Abubakar Jamil <abubakarjamil@gmail.com>
2019-08-04 13:46:33 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,18 +3,19 @@ function basicTeenager(age){
 }
 
 function teenager(age) {
-  if age >= 13 && age <= 19 {return "You are a teenager!";}
+  if (age >= 13 && age <= 19) {return "You are a teenager!";}
     else {return "You are not a teenager";}
 }
 
 function ageChecker(age) {
-  if age >= 13 && age <= 19 {return "You are a teenager!";}
+  if (age >= 13 && age <= 19) {return "You are a teenager!";}
     else if (age<=12) {return "You are a kid";}
     else {return "You are a a grownup"}
 }
 
 function ternaryTeenager(age) {
-
+  return (age >=13 && age <=19) ?|
+  "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
