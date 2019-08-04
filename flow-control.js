89222e2c7ae2ec490f20429629afedd7ee542e48
Abubakar Jamil <abubakarjamil@gmail.com>
2019-08-04 13:19:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,8 +5,13 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+  if age >= 13 && age <= 19 {
+  return "You are a teenager!";
+} else {
+  "You are not a teenager";
 }
+
+
 
 function ageChecker(age) {
 
